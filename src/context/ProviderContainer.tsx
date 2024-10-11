e8b55b865dba3dd9f2916c15021219b685603d47
serkancan <serkancan@DESKTOP-4KCP4EG>
2024-10-11 14:48:36 +0300

    progress top bar

--- src/context/ProviderContainer.tsx
+++ src/context/ProviderContainer.tsx
@@ -1,12 +1,28 @@
+import React, { ReactNode } from "react";
 import { HotToastContainer } from "@/services/notifications/HotToastrService";
 import { ToastifyContainer } from "@/services/notifications/ToastifyService";
 import CustomThemeProvider from "@/themes/CustomThemeProvider";
-import React, { ReactNode } from "react";
+import NextTopLoader from "nextjs-toploader";
 
 const ProviderContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <React.Fragment>
       <CustomThemeProvider>
+        <NextTopLoader
+          color="#22C55E"
+          initialPosition={0.08}
+          crawlSpeed={300}
+          height={4}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <ToastifyContainer />
         <HotToastContainer />
         {children}
